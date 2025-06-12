464c2ef63694407e9c94f3bc2d79dccff1b7fc0b
Connor <none>
2025-06-12 12:15:11 -0600

    and... its gone

---
+++ src/param.h
@@ -0,0 +1,28 @@
+
+#include "point.h"
+
+typedef struct Param_s{
+default_random_engine generator;
+
+int maxiterations=100;
+int num = 2;
+int CROSSBOUND = 100;
+double majorAxis = 0.75;
+double minorAxis = 0.75;
+double DIST = 36;
+double THRESHOLD = exp (-majorAxis*majorAxis);
+double alpha;
+double epsilon, INF;
+double varX, varY;
+
+vector<double> areas, lengths, angles, eps;
+FILE *out = fopen ("../sketch_plot.txt", "w");
+
+
+Point drone_start_B, drone_start_A, drone_start_AB, drone_start_BB;
+vector<Point> gaussianCenter;
+vector<Point> gaussianVar;
+
+
+int currentGaussian1;
+} Param;
